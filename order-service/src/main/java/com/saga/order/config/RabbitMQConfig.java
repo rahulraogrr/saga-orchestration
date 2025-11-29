0bcbb424323d34c0bca147ab9222cee135e968ca
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2025-11-29 09:20:55 +0000

    refactor: use MessageConverter interface and add NonNull annotations for RabbitMQ configuration

--- order-service/src/main/java/com/saga/order/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/saga/order/config/RabbitMQConfig.java
@@ -7,8 +7,10 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class RabbitMQConfig {
@@ -92,13 +94,14 @@ public class RabbitMQConfig {
 
     // Message Converter
     @Bean
-    public Jackson2JsonMessageConverter messageConverter() {
+    @NonNull
+    public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
     // RabbitTemplate
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    public RabbitTemplate rabbitTemplate(@NonNull ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter());
         return template;
